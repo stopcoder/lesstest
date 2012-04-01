48f9b41bf9472e1a5ceb349fdc7e6c3b980408ae
Jiawei Cao <stopcoder@gmail.com>
2012-04-02 00:39:53 +0200

    variable setting is working

--- lesstest/WebContent/loadingless.js
+++ lesstest/WebContent/loadingless.js
@@ -1,16 +1,37 @@
 (function(window, document){
-	console.log(less.tree);
+	//console.log(less.tree);
+	
+	
+	var scrollerControl = function(id){
+		var node = $(id);
+		var unit = node.width() / 16;
+		
+		node.bind("mousedown",function(){
+			
+		});
+	}
 	
 	$(document).ready(function(){
 		
 		var parser = new less.Parser();
 		
+		function cssToDocument(cssText, id){
+			if(id){
+				$("#"+id).html(cssText);
+			}else{
+				id = 'less_calc';
+				$("head").append($("<style id="+id+"></style>").html(cssText));
+			}
+			return id;
+		}
+		
 		$.get("less/test.less", function(data){
-			console.log(data);
+			//console.log(data);
 			parser.parse(data, function(error, tree){
-				console.log(tree);
-				var cssCode = tree.toCSS({},{ greyColor: new less.tree.Color('#ffffff') });
-				console.log(cssCode);
+				//console.log(tree);
+				var cssCode = tree.toCSS({},{ greyColor: new less.tree.Color('FF0000') });
+				//console.log(cssCode);
+				cssToDocument(cssCode);
 			});
 		});
 	});
